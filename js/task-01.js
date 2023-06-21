52300cb42bb25278bbfe3c3f31cffef9062f710c
Volodymyr <buvoro@gmail.com>
2023-06-21 08:56:04 +0300

    перший

--- js/task-01.js
+++
@@ -1,6 +0,0 @@
-const listItemsEl = document.querySelectorAll('.item ul');
-console.log('Number of categories: ' + listItemsEl.length);
-listItemsEl.forEach((item) => {
-    console.log('Category: ' + item.previousElementSibling.textContent);
-    console.log('Elements: ' + item.children.length);
-})
--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-const inputRangeEl = document.getElementById('font-size-control');
-const spanEl = document.getElementById('text');
-inputRangeEl.addEventListener('input', changeFontSize);
-
-function changeFontSize() {
-    spanEl.style.fontSize = inputRangeEl.value + 'px';
-}
