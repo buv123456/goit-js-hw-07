94c5939c5eb26809abf6d8687c595e5817472b95
Volodymyr <buvoro@gmail.com>
2023-06-22 02:08:45 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const listEl = document.querySelector('.gallery');
 listEl.insertAdjacentHTML('afterbegin', addImg(galleryItems))
 let instance = '';
@@ -22,36 +20,25 @@ function addImg(arr) {
     ).join('')
 }
 
-// ----------------------------------------------------
 listEl.addEventListener('click', onClick);
 
 function onClick(event) {
     event.preventDefault();
     if (event.target === event.currentTarget) return;
-    const objImg = findImgObj(event.target.src)
-    instance = basicLightbox.create(makeImgMarkup(objImg), {
-        closable: false
-    })
-    instance.show();
-    window.addEventListener('keydown', closeMarkup);
+    const objImg = galleryItems.find(({ preview } = {}) => preview === event.target.src);
+    instance = basicLightbox.create(makeImgMarkup(objImg));
+    instance.show(window.addEventListener('keydown', closeMarkup));
 }
 
-function findImgObj(imgTarget) {
-    return galleryItems.find(({ preview } = {}) => preview === imgTarget)
-}
-
-function makeImgMarkup({ original }) {
+function makeImgMarkup({ original, description }) {
     return `
     <img
-      class="gallery__image"
       src="${original}"
-      alt="Image description"
+      alt="${description}"
     />`
 }
 
 function closeMarkup(event) {
-    if (event.code !== "Escape") return;
-    instance.close();
-    window.removeEventListener('keydown', closeMarkup);
+    if (event.code === "Escape") instance.close(window.removeEventListener('keydown', closeMarkup));
 }
 
